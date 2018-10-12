41618a81c8bd73d7fe35cce5c9deacfdbd6c67c7
CodeDraken <codedraken@users.noreply.github.com>
2018-10-11 22:51:52 -0500

    Basic setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+
 # electron-file-metadata
+
 File metadata viewer made with Electron.
---
+++ main.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+
+// declare this as a variable globally so we can
+// reference it and so it will not be garbage collected
+let mainWindow
+
+// wait for the main process to be ready
+app.on('ready', () => {
+  // path to our html
+  const htmlPath = path.join('src', 'index.html')
+
+  console.log(htmlPath)
+
+  // create a browser window
+  mainWindow = new BrowserWindow()
+
+  mainWindow.loadFile()
+})
